953942df9f4a66138dfcd20c12bbee7312714afe
陈杨 <yang.chen@w-group.com.cn>
2022-07-06 15:31:47 +0800

    test

--- resources/views/banner_index.blade.php
+++ resources/views/banner_index.blade.php
@@ -1,16 +1,30 @@
-<link href="{{ asset('/vendor/admin_banner/css/swiperConfig.css')  }}" rel="stylesheet" type="text/css">
-<div id="app">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>轮播图配置</title>
+    <link rel="stylesheet" href="../assets/libs/element.css">
+    <link rel="stylesheet" href="../assets/swiperConfig.css">
+<!--  <link href="{{ asset('/vendor/admin_banner/css/swiperConfig.css')  }}" rel="stylesheet" type="text/css">-->
+</head>
+<body>
+<div id="app-a328">
     <div class="wrapper">
         <div class="title">页面布局</div>
         <div class="content">
             <div class="phone-modal">
                 <div class="status-bar">
                     <div class="time">13:14</div>
-                    <img src='{{ asset('/vendor/admin_banner/images/status-bar.png')  }}' class="status-bar-img">
+
+                    <img src="{{ asset('/vendor/admin_banner/images/status-bar.png')  }}" class="status-bar-img">
                 </div>
-                <img src='{{ asset('/vendor/admin_banner/images/mini-icon.png')  }}' class="mini-icon">
+
+                <img src="{{ asset('/vendor/admin_banner/images/mini-icon.png')  }}" class="mini-icon">
                 <el-carousel trigger="click" class="swiper" height="550px" :autoplay="radio == 2" loop indicator-position="none" ref="swiperRef">
                     <el-carousel-item v-for="(item,index) in configList" :key="index">
+
                         <img src="{{ asset('/vendor/admin_banner/images/person-info.png')  }}" class="img" alt="">
                     </el-carousel-item>
                 </el-carousel>
@@ -39,9 +53,9 @@
                         <vuedraggable v-model="configList">
                             <div class="list-item" v-for="(item,index) in configList" :key="index">
                                 <el-popconfirm
-                                    title="确定删除？"
-                                    @confirm="handleDelete(index)"
-                                    class="delete-icon"
+                                        title="确定删除？"
+                                        @confirm="handleDelete(index)"
+                                        class="delete-icon"
                                 >
                                     <i slot="reference" class="el-icon-delete icon"></i>
                                 </el-popconfirm>
@@ -49,11 +63,11 @@
                                 <div class="title-wrapper">
                                     <div class="left"><span class="required">素材：</span></div>
                                     <el-upload
-                                        action="https://jsonplaceholder.typicode.com/posts/"
-                                        list-type="picture-card"
-                                        :limit="1"
-                                        :on-success="(args)=>handleUpLoadSuccess(index,...args)"
-                                        :show-file-list="false"
+                                            action="https://jsonplaceholder.typicode.com/posts/"
+                                            list-type="picture-card"
+                                            :limit="1"
+                                            :on-success="(args)=>handleUpLoadSuccess(index,...args)"
+                                            :show-file-list="false"
                                     >
                                         <img v-if="item.url" :src="item.url" class="avatar">
                                         <i v-else class="el-icon-plus avatar-uploader-icon"></i>
@@ -67,10 +81,10 @@
                                     <div class="left"><span class="required">交互类型：</span></div>
                                     <el-select v-model="item.type" placeholder="请选择" size="mini" style="flex:1;">
                                         <el-option
-                                            v-for="item in options"
-                                            :key="item.value"
-                                            :label="item.label"
-                                            :value="item.value">
+                                                v-for="item in options"
+                                                :key="item.value"
+                                                :label="item.label"
+                                                :value="item.value">
                                         </el-option>
                                     </el-select>
                                 </div>
@@ -98,68 +112,70 @@
             </div>
         </div>
     </div>
-</div>
 
-<script src="{{asset('/vendor/admin_banner/js/vue.js')}}"></script>
-<script src="{{asset('/vendor/admin_banner/js/element.js')}}"></script>
-<script src="{{asset('/vendor/admin_banner/js/Sortable.min.js')}}"></script>
-<script src="{{asset('/vendor/admin_banner/js/vuedraggable.umd.min.js')}}"></script>
-<script src="{{asset('/vendor/admin_banner/js/axios.min.js')}}"></script>
-<script>
-    Vue.component('vuedraggable', vuedraggable)
-    // console.log(axios);
-    new Vue({
-        el: '#app',
-        data(){
-            return {
-                autoplay:false,
-                interval:3000,
-                radio:1,
-                input:'',
-                configList:[
-                    {
-                        type:'none',
-                    }
-                ],
-                options: [{
-                    value: 'none',
-                    label: '无'
-                }, {
-                    value: 'official',
-                    label: '公众号文章'
-                }, {
-                    value: 'outside_mini',
-                    label: '其他小程序'
-                }, {
-                    value: 'inside_mini',
-                    label: '跳转内部页面'
-                }]
-            }
-        },
-        mounted() {
-            // setTimeout(()=>{
-            //   this.$refs.swiperRef.prev()
-            // },1000)
+ <script src="{{asset('/vendor/admin_banner/js/vue.js')}}"></script>
+ <script src="{{asset('/vendor/admin_banner/js/element.js')}}"></script>
+  <script src="{{asset('/vendor/admin_banner/js/Sortable.min.js')}}"></script>
+    <script src="{{asset('/vendor/admin_banner/js/vuedraggable.umd.min.js')}}"></script>
+  <script src="{{asset('/vendor/admin_banner/js/axios.min.js')}}"></script>
 
-        },
-        methods: {
 
-            addConfig(){
-                this.configList.push({type:'none'})
+    <script>
+        Vue.component('vuedraggable', vuedraggable)
+        // console.log(axios);
+        new Vue({
+            el: '#app-a328',
+            data(){
+                return {
+                    autoplay:false,
+                    interval:3000,
+                    radio:1,
+                    input:'',
+                    configList:[
+                        {
+                            type:'none',
+                        }
+                    ],
+                    options: [{
+                        value: 'none',
+                        label: '无'
+                    }, {
+                        value: 'official',
+                        label: '公众号文章'
+                    }, {
+                        value: 'outside_mini',
+                        label: '其他小程序'
+                    }, {
+                        value: 'inside_mini',
+                        label: '跳转内部页面'
+                    }]
+                }
             },
-            handleDelete(index){
-                // console.log(index);
-                this.configList.splice(index,1)
-            },
-            handleCancel(){},
-            saveConfig(){
-                console.log(this.configList,this.list);
-            },
-            handleUpLoadSuccess(index,res, file){
-                console.log(index,res,file);
-            }
-        },
-    })
+            mounted() {
+                // setTimeout(()=>{
+                //   this.$refs.swiperRef.prev()
+                // },1000)
 
-</script>
+            },
+            methods: {
 
+                addConfig(){
+                    this.configList.push({type:'none'})
+                },
+                handleDelete(index){
+                    // console.log(index);
+                    this.configList.splice(index,1)
+                },
+                handleCancel(){},
+                saveConfig(){
+                    console.log(this.configList,this.list);
+                },
+                handleUpLoadSuccess(index,res, file){
+                    console.log(index,res,file);
+                }
+            },
+        })
+
+    </script>
+</body>
+</html>
